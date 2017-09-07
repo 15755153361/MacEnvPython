711a19a1afca89d1ed87930060f25fd08ae2f95c
“Sangewang” <w_lin1025@163.com>
2017-09-08 07:14:52 +0800

    add unitest

--- July/BasicPython/6_HighLevelOOP/lesson_06_utt.py
+++ July/BasicPython/6_HighLevelOOP/lesson_06_utt.py
@@ -4,10 +4,19 @@ class MyDict(dict):
     pass
 
 class TestMyDict(unittest.TestCase):
+    def setUp(self):
+        print('Test Begin')
+
+    def tearDown(self):
+        print('Test End')
+
     def test_init(self):
         md = MyDict(one=1,two=2)
         self.assertEqual(md['one'],1)
         self.assertEqual(md['two'],2)
+    
+    def test_nonthing(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
