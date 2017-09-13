a3753f4e0adc56918d35661c88b795dee7a7e1ad
“Sangewang” <w_lin1025@163.com>
2017-09-14 07:15:26 +0800

    filter

---
+++ July/BasicPython/7_AllThread/filter.py
@@ -0,0 +1,10 @@
+number_list = list(range(-5,5))
+print('type of list(range()) = ',type(number_list),'number_list = ',number_list)
+less_than_zero = list(filter(lambda x : x<0,number_list))
+print(less_than_zero)
+
+
+
+
+
+
