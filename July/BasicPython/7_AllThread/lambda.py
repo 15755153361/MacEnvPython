1082bba63d0168a1f20d21af65bb00cae9f318ba
“Sangewang” <w_lin1025@163.com>
2017-09-13 08:22:00 +0800

    add func program

---
+++ July/BasicPython/7_AllThread/lambda.py
@@ -0,0 +1,6 @@
+g = lambda x : x * 2
+print(g(3))
+print((lambda x : x * 2)(4))
+
+sum_lambda = lambda x,y : x + y
+print(sum_lambda(10,20))
---
+++ July/BasicPython/7_AllThread/map.py
@@ -0,0 +1,7 @@
+name_list = ['practice','for','funcmap']
+for n in name_list:
+    print(len(n))
+
+name_len = list(map(len,['ni','hao','funPro']))
+print(type(name_len),name_len)
+
