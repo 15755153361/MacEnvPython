20c3f6c672fe74465c07c2cd02e74e92a0724a9c
“Sangewang” <w_lin1025@163.com>
2017-09-13 08:29:54 +0800

    add lambda and map

--- July/BasicPython/7_AllThread/map.py
+++ July/BasicPython/7_AllThread/map.py
@@ -5,3 +5,18 @@ for n in name_list:
 name_len = list(map(len,['ni','hao','funPro']))
 print(type(name_len),name_len)
 
+def toUpper(item):
+    return item.upper()
+
+upper_name = list(map(toUpper,name_list))
+print(upper_name)
+print('-----------------------------------------')
+items = list(range(1,6,1))
+
+squared = []
+for i in items:
+    squared.append(i**2)
+print('squared = ',squared)
+
+squared_new = list(map(lambda x: x**2,items))
+print('squared_new = ',squared_new)
